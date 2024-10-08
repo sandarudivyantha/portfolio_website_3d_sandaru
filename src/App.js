732b87a4e3684f4e28ca0abfcb951ab759d8e6e5
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-10-08 20:06:44 +0530

    uploading project that I've done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/sections/Dashboard";
+import ProfilesSection from "./components/sections/ProfilesSection";
+import Skills from "./components/sections/Skills";
+import Education from "./components/sections/Education"
+import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
+import Footer from "./components/sections/Footer";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+  position: relative;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+          <div>
+            <Dashboard />
+            <ProfilesSection />
+            <Skills />
+            <Education/>
+            <Projects/>
+            <Contact/>
+            <Footer/>
+          </div>
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
